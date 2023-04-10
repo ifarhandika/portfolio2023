4856377b3b66d44664d18c446fdbed6699b8d211
ifarhandika <ifarhandika@gmail.com>
2023-04-10 16:52:44 +0700

    build mvp1

--- src/components/Pages.jsx
+++ src/components/Pages.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { Link, NavLink, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 
 import Home from "../pages/Home"
 import Info from "../pages/Info"
